92ff79436dec03a2fbec99d47c95332027e91ae9
Taffy Dzomba <dzombataffy@gmail.com>
2023-11-02 08:20:22 +0000

    # Updating local provider

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -5,7 +5,7 @@ provider "local.providers/local/terratowns" {
   version     = "1.0.0"
   constraints = "1.0.0"
   hashes = [
-    "h1:1mNQE0KmV+kp62PcGYKAVFy/qmLFYOOoRKOEyezYcL8=",
+    "h1:zJ1t1t2WFp6/wNk/bAbIC3cF9iyoACn+lnsBos1Qlwc=",
   ]
 }
 
--- main.tf
+++ main.tf
@@ -1,14 +1,13 @@
 terraform {
- required_providers {
-   terratowns = {
+  required_providers {
+    terratowns = {
       source  = "local.providers/local/terratowns"
       version = "1.0.0"
- } 
-
+    }
+  }
 }
-}
 
- provider "terratowns" {
+provider "terratowns" {
   endpoint  = var.terratowns_endpoint
   user_uuid = var.teacherseat_user_uuid
   token     = var.terratowns_access_token
@@ -23,3 +22,18 @@ terraform {
 #    index_html_file_path = var.index_html_file_path
 #    public_path = var.public_path
 #  }
+
+
+resource "terratowns_home" "home" {
+name = "How to play Compass Game"
+  description = <<DESCRIPTION
+- Type commands to interact with the game. For example, you can type "go north," "examine the chest," or "talk to the merchant."
+- Your choices matter! Think carefully before making decisions.
+- Save your progress by typing "save" at any time.
+- To quit the game, type "quit."
+Enjoy your adventure, and may you find the treasures that await you in AdventureQuest!
+DESCRIPTION
+  domain_name = module.home_compass.domain_name
+  town = "missingo"
+  content_version = var.home_compass.content_version
+}
